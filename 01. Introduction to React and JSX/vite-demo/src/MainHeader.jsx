b57447caa32fca9cc9d0d73cda3a2047c5a53bbc
Ioan Velev <ioanvelev46@gmail.com>
2024-07-27 18:25:42 +0300

    Update components

---
+++ 01. Introduction to React and JSX/vite-demo/src/MainHeader.jsx
@@ -0,0 +1,7 @@
+//import React from "react";
+
+export default function MainHeader(){
+    // return React.createElement('h1', null, 'Custom Compononent');
+
+    return <h2>Custom component with JSX</h2>
+}
