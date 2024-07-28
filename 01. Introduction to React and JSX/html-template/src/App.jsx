5e8510ffdb6a8b8599185ef397e983e2afadaa87
Ioan Velev <ioanvelev46@gmail.com>
2024-07-28 23:23:39 +0300

    Create demo with html template

---
+++ 01. Introduction to React and JSX/html-template/src/App.jsx
@@ -0,0 +1,20 @@
+import Home from "./components/Home";
+import Services from "./components/Services";
+import About from "./components/About";
+import MoreInfo from "./components/MoreInfo";
+import Team from "./components/Team";
+import Footer from "./components/Footer";
+function App() {
+return (
+    <>
+      <Home/>
+      <Services/>
+      <About/>
+      <MoreInfo/>
+      <Team/>
+      <Footer/>
+    </>
+    );
+}
+
+export default App;
