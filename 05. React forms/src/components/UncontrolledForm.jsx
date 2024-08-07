58503baedb5acffbb5e1475d5a9ce15454b730bb
Ioan Velev <ioanvelev46@gmail.com>
2024-08-07 18:06:35 +0300

    Update UncontrolledForm.jsx

--- 05. React forms/src/components/UncontrolledForm.jsx
+++ 05. React forms/src/components/UncontrolledForm.jsx
@@ -1,8 +1,19 @@
 export default function UncontrolledForm() {
+    const formSubmitHanlder = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        console.log(formData.get('username'));
+        
+
+        console.log('Form Submit');
+        
+    }
+
   return (
     <>
       <h1>Uncontrolled Form</h1>
-      <form>
+      <form onSubmit={formSubmitHanlder}>
         <div>
           <label htmlFor="username">Username</label>
           <input type="text" name="username" id="username" />
