049be6db645c9f7c8222bf9f7fa4ab3763d99e7d
Ioan Velev <ioanvelev46@gmail.com>
2024-08-11 20:02:06 +0300

    Update Articles.jsx

--- 06. React routing/client/src/components/Articles.jsx
+++ 06. React routing/client/src/components/Articles.jsx
@@ -6,12 +6,19 @@ import { Link } from 'react-router-dom'
 
 
     useEffect(() => {
+      const abortController = new AbortController();
       (async() => {
-        const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list');
+        const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list', {
+          signal: abortController.signal
+        });
         const result = await response.json();
 
         setArticles(result);
       })();
+
+      return () => {
+        abortController.abort();
+      };
     }, []);
 
     return (
