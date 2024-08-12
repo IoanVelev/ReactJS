091335784cdf7b54120b8bb3d4dbf10085fdb22b
Ioan Velev <ioanvelev46@gmail.com>
2024-08-12 18:03:20 +0300

    Add new components

--- 07. React hooks/client/src/App.jsx
+++ 07. React hooks/client/src/App.jsx
@@ -1,13 +1,34 @@
+import { useFetch } from "./hooks/useFetch";
 
-import './App.css'
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ArticleCard from "./components/ArticleCard";
+import NavBar from "./components/NavBar";
+import styles from "./App.module.css";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 function App() {
+  const {
+    data: articles,
+    isFetching,
+    refetch,
+  } = useFetch("http://localhost:3030/jsonstore/advanced/articles/list", []);
 
   return (
     <>
-      
+      <NavBar />
+      {isFetching ? (
+        <Spinner />
+      ) : (
+        <div className={styles["article-list"]}>
+          {articles.map((article) => (
+            <ArticleCard key={article._id} {...article} />
+          ))}
+        </div>
+      )}
+      <Button variant="primary" onClick={refetch}>Refresh</Button>{" "}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
